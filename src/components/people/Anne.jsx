09833d17401a2bc5242aee27318ba8bf2cb965c4
dbx834 <orsha.pk@gmail.com>
2019-08-06 13:23:33 +0530

    patch

--- src/components/people/Anne.jsx
+++ src/components/people/Anne.jsx
@@ -23,35 +23,33 @@ const Person = ({ show = true }) => (
       Anne Terlouw
       <Fragment>{show === true && ' – Apprentice'}</Fragment>
     </h1>
-    <p>
-      <i>
-        Holland has been my home where I was born and raised. I’m naturally
-        drawn towards supporting others by creating a space where they can
-        connect back with their inner self. I’m there to make them more aware of
-        themselves, whether this is through movement, meditation or a
-        conversation. When I arrived in Auroville beginning 2019 I discovered a
-        new dimension: our way of listening. Focusing on this so we can connect
-        on a deeper level behind the words, through our heart.
-      </i>
-    </p>
-    <p>
-      <i>
-        My need for a deeper connection with my own feelings brought me to
-        L’aura and the Restorative Auroville team. Through applying Restorative
-        Circles in our community we can create a space where we support each
-        other back to our inner being. From there we can’t only hear our own
-        needs, but also hear the needs of the other side more clearly. I believe
-        that when we listen to the deeper meaning behind the words we can find
-        the Unity that is already within all of us. With experiencing this Unity
-        we can find the beauty in our conflicts, they’re only there to make us
-        more aware.
-      </i>
-    </p>
     <p style={{ marginBottom: 0 }}>
       <i>
-        I feel blessed to have the opportunity to contribute and learn through
-        this process towards a justice system for Auroville where connection is
-        more important than punishment.
+        I was born and raised in Holland, where life brought me to study
+        Architectural Engineering. After a few years I felt this study wasn’t
+        giving me the life energy and peace that it could give me. To become
+        inspired again I felt the longing to travel. A yoga teacher training,
+        Vipassana and exploring India and Nepal made me discover a new side of
+        myself: a calm, loving and more aware one. I felt that I’m naturally
+        drawn towards supporting others, and that to facilitate that space, I
+        should start with taking good care of myself first. Back home I started
+        working as a restaurant manager and barista, and next to that I could
+        delve into different ways of practicing and teaching yoga and
+        meditation. I got inspired by different forms of movement and
+        meditation, photography, Shamanism and Qi Gong, and when I arrived in
+        Auroville in the beginning of 2019 another dimension came up for me: a
+        new way of listening. Exploring my way of listening through following
+        the Integral Sound Healing Training at Svaram, I noticed a deeper
+        longing for connection with my own feelings and needs. That’s why
+        meeting L’aura and the Restorative Auroville team feels like a natural
+        next step in this always evolving life. I feel blessed to combine this
+        life experience with Nonviolent Communication so we can create a space
+        where we support each other back to our inner being. Through applying
+        Restorative Circles in our community we can hear our own and others'
+        needs more clearly. By listening to the deeper meaning behind the words
+        we can find the Unity that is already within us. With experiencing this
+        Unity we can find the beauty in every conflict. I believe that conflict
+        is only inviting us to become more aware of ourselves.
       </i>
     </p>
   </div>
