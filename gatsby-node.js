325eb87e80fff4e8acad02822884d63e29278cab
dbx834 <orsha.pk@gmail.com>
2020-01-11 04:50:19 +0530

    test#8

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,6 +14,19 @@ const markdown = require('remark-parse')
 // ----------------------------------------------------------------------------
 // ------------------------------------------------------------------ Functions
 // ----------------------------------------------------------------------------
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
+
 // exports.onCreateWebpackConfig = ({ stage, actions }) => {
 //   actions.setWebpackConfig({
 //     node: false,
